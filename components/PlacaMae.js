cf4db22477b04693ca6cd47de058caf1af49c39b
Vinicius <vinicius_apple22@otlook.com>
2025-03-10 22:32:52 -0300

    colocando imagens

--- components/PlacaMae.js
+++ components/PlacaMae.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function PlacaMae() {
   return (
     <View style={styles.container}>
-      <Text>Placa Mãe</Text>
+      <Text style={styles.titulo}>Placa Mãe</Text>
+      
+      <View>
+      <Image
+            style={{resizeMode: 'center'}}
+            source={require('../assets/MOBO.jpg')}
+          />
+      </View>
+
+      <Text style={styles.subTitulo}>A placa mãe é o componente central de um computador, responsável por interligar todos os outros componentes do sistema, como o processador, a memória RAM, o armazenamento e as placas de expansão. Ela permite que esses componentes se comuniquem entre si e com o exterior, por meio de portas USB, HDMI, e outras interfaces. Além disso, a placa mãe também oferece conectividade para dispositivos externos e pode incluir funcionalidades extras, como áudio e rede.</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  container:{
+    flex:1,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f4f4f4',
+    justifyContent: 'center'
   },
+  titulo:{
+      fontSize:30
+  },
+  subTitulo:{
+      fontSize:15
+  }
 });
--- components/PlacaVideo.js
+++ components/PlacaVideo.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function PlacaVideo() {
   return (
     <View style={styles.container}>
       <Text>Placa de Video</Text>
+      <Image style={{resizeMode: 'center'}} source={require('../assets/GPU.jpg')} />
       <StatusBar style="auto" />
+      <Text>A placa de vídeo, também conhecida como GPU (Unidade de Processamento Gráfico), é responsável pelo processamento e renderização das imagens e vídeos que aparecem no monitor. Ela alivia o processador principal, o CPU, ao lidar com tarefas gráficas complexas, como jogos, design gráfico e edição de vídeos. Placas de vídeo mais avançadas possuem seu próprio processador e memória, otimizando o desempenho gráfico e permitindo uma melhor experiência visual.</Text>
     </View>
   );
 }
